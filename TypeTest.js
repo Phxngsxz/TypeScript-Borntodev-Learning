f8ba3bfd43956223faea654b2f1433dce983a7fe
Kagenou <phongsaphon.ssi@gmail.com>
2025-03-25 15:16:05 +0700

    data type

---
+++ TypeTest.js
@@ -0,0 +1,9 @@
+var myName = "Phongsaphon somsin";
+console.log(myName);
+console.log(typeof (myName));
+var money = 100;
+console.log(money);
+console.log(typeof (money));
+console.log(null === undefined);
+console.log(null == undefined);
+console.log(null === null);
---
+++ TypeTest.ts
@@ -0,0 +1,15 @@
+var myName: string = "Phongsaphon somsin";
+
+console.log(myName);
+console.log(typeof(myName));
+
+var money: number = 100;
+console.log(money);
+console.log(typeof(money));
+
+console.log(null === undefined);
+console.log(null == undefined);
+console.log(null === null);
+
+
+
